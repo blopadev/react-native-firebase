629c5495aeb24875203304ab6fcf7c1a2d39f09e
Pablo Martin <pabloenjerez@gmail.com>
2021-06-10 16:55:58 +0200

    Creo El Stack Navigator con 3 pantallas (hasta 21:25)
    	modificado:     App.js
    	modificado:     package-lock.json
    	modificado:     package.json
    	nuevo archivo:  screens/CreateUserScreen.js
    	nuevo archivo:  screens/UserDetailScreen.js
    	nuevo archivo:  screens/UsersList.js

---
+++ screens/CreateUserScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const CreateUserScreen = () => {
+  return (
+    <View>
+      <Text>Create User </Text>
+    </View>
+  );
+};
+
+export default CreateUserScreen;
