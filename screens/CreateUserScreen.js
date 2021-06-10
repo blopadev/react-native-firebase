8b19cdb65c3e3fd866ed3bb1ef08bf92787d18cb
Pablo Martin <pabloenjerez@gmail.com>
2021-06-10 18:06:43 +0200

    Guardo los datos del formulario en el estado y al pulsar el botón muestro el estado por consola (hasta 28:59)
    	modificado:     screens/CreateUserScreen.js

--- screens/CreateUserScreen.js
+++ screens/CreateUserScreen.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Button, TextInput, ScrollView, StyleSheet } from 'react-native';
 
 
 const CreateUserScreen = () => {
+
+
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
+  const handleChangeTtext = (name, value) => {
+    setState({ ...state, [name]: value })
+  }
+
   return (
     <ScrollView style={styles.container} >
       <View style={styles.inputGroup} >
-        <TextInput placeholder="User Name" />
+        <TextInput
+          placeholder="User Name"
+          onChangeText={(value) => handleChangeTtext('name', value)}
+        />
       </View>
-      <View style={styles.inputGroup}>
-        <TextInput placeholder="User Email" />
+      <View style={styles.inputGroup} >
+        <TextInput
+          placeholder="User Email"
+          onChangeText={(value) => handleChangeTtext('email', value)}
+        />
       </View>
-      <View style={styles.inputGroup}>
-        <TextInput placeholder="User Phone" />
+      <View style={styles.inputGroup} >
+        <TextInput
+          placeholder="User Phone"
+          onChangeText={(value) => handleChangeTtext('phone', value)}
+        />
       </View>
       <View>
-        <Button title="Save User" />
+        <Button title="Save User" onPress={() => console.log(state)} />
       </View>
     </ScrollView>
   );
