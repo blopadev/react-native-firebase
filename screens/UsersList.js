4f04980856fb41b3ccec21f7d912c7f7c1f9d55a
Pablo Martin <pabloenjerez@gmail.com>
2021-06-12 13:47:12 +0200

    Veo la lsita de usaurios formateada y creo un botón que muestra una alerta con el id  de usuario (hasta 57:54) 	modificado:     App.js 	modificado:     package-lock.json 	modificado:     package.json 	modificado:     screens/UsersList.js

--- screens/UsersList.js
+++ screens/UsersList.js
@@ -1,24 +1,63 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView, Button } from 'react-native';
 import firebase from "../database/firebase";
+import { ListItem, Avatar } from "react-native-elements";
 
 
-const UsersList = () => {
+const UsersList = (props) => {
 
   const [users, setUsers] = useState([])
 
   useEffect(() => {
     firebase.db.collection('users').onSnapshot(querySnapshot => {
+      const users = [];
+
       querySnapshot.docs.forEach(doc => {
-        console.log(doc.data())
-      })
+        const { name, email, phone } = doc.data()
+        users.push({
+          id: doc.id,
+          name,
+          email,
+          phone
+        })
+      });
+
+      setUsers(users)
+
     })
   }, [])
 
   return (
-    <View>
-      <Text>Users List</Text>
-    </View>
+
+    <ScrollView>
+      <Button
+        title="Create User"
+        onPress={() => props.navigation.navigate('CreateUserScreen')}
+      />
+
+      {
+        users.map(user => {
+          return (
+            <ListItem
+              key={user.id}
+              bottomDivider // aparece una líne bajo ls tarjetas
+              onPress={() => alert(user.id)}
+            >
+              <ListItem.Chevron />
+              <Avatar
+                source={{ uri: 'https://reactnativeelements.com/img/avatar/avatar--photo.jpg' }}
+                rounded
+              />
+
+              <ListItem.Content>
+                <ListItem.Title>{user.name}</ListItem.Title>
+                <ListItem.Subtitle>{user.email} </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          )
+        })
+      }
+    </ScrollView>
   );
 };
 
